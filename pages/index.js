e6753e8716d572d6e820348df1faca6d92dc6799
Rovinya Wijerama <rovinyawijeramaofficial@gmail.com>
2024-11-18 09:11:03 +0530

    Initialized

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+function HomePage() {
+  return (
+    <div>
+      <h1>The Home Page</h1>
+      <form>
+        <div>
+        <label htmlFor="email">Your Email Address</label>
+        <input type="email" id="email"/>
+        </div> 
+        <div>
+        <label htmlFor="email">Your Email Address</label>
+        <input type="email" id="email"/>
+        </div>  
+      </form>
+    </div>
+  );
+}
+
+export default HomePage;
